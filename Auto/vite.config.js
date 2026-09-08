392895e4f792be5b002cf3853799ec5ef56df141
Bossbrown1770 <frankrooney474@gmail.com>
2026-09-08 14:19:46 +0100

    Update vite.config.js

--- Auto/vite.config.js
+++ Auto/vite.config.js
@@ -1,13 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
   logLevel: 'error',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',    // maps @ to the /src directory (relative to root)
     },
   },
 });
